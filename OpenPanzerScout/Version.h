a6645756542b0365908c542d10c175acfad00f9e
Open Panzer <admin@openpanzer.org>
2018-07-01 19:56:56 -0500

    Update to Version 0.91.10, this version is only compatible with Scout boards Rev 11 and later. For Scout boards Rev 10 and earlier, use firmware Version 0.90.03

---
+++ OpenPanzerScout/Version.h
@@ -0,0 +1,13 @@
+// Version.h        Open Panzer Scout ESC Firmware Version
+ 
+#ifndef VERSION_H
+#define VERSION_H
+
+
+#define FIRMWARE_VERSION        "0.91.10"      // Last update 7/1/2018
+
+// NOTE:
+// Versions past 0.91.xx are for Rev 11 and later boards only
+// Version 0.90.03 from 3/11/2017 is the last version of firmware compatible with Rev 10 and earlier boards
+
+#endif
