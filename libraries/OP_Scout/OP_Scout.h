14eb716f84c0f05d9395b8ad85340ab2a9c251fd
Open Panzer <admin@openpanzer.org>
2018-07-03 20:02:05 -0500

    Add some extra notes to example sketch, and a convenience function to set current limits.

--- libraries/OP_Scout/OP_Scout.h
+++ libraries/OP_Scout/OP_Scout.h
@@ -48,6 +48,10 @@
 #define SCOUT_ADDRESS_A                     0x83    // 131
 #define SCOUT_ADDRESS_B                     0x84    // 132
 
+// Defaults
+#define SCOUT_DEFAULT_CURRENT_LIMIT         12      // 12 amps by default per motor, this is the amount it should be able to maintain continuously without extra cooling
+#define SCOUT_MAXIMUM_CURRENT_LIMIT         30      // 30 amps maximum current per motor
+
 // Commands                                         // 0    Motor 1 forward
                                                     // 1    Motor 1 reverse
                                                     // 2    Set minimum voltage
@@ -123,7 +127,13 @@ public:
   
     // Drag inner track
     inline void DragInnerTrack(boolean enable) { command(SCOUT_CMD_DRAG_INNER_TRACK, enable); }    
-  
+
+    // Set maximum current
+    inline void SetMaxCurrent(uint8_t cl) 
+    { 
+        if (cl > 0 && cl <= SCOUT_MAXIMUM_CURRENT_LIMIT) command(SCOUT_CMD_SET_MAX_CURRENT, cl); 
+        else                                             command(SCOUT_CMD_SET_MAX_CURRENT, SCOUT_DEFAULT_CURRENT_LIMIT);
+    }  
 
 private:
     void throttleCommand(byte command, int speed) const;
