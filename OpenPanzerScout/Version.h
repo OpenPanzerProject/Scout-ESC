b3c24fe2bbcd34ea6b1bea36ea7cd11ec004f37d
Open Panzer <admin@openpanzer.org>
2019-04-30 14:43:29 -0500

    Update to Versio 0.91.11

--- OpenPanzerScout/Version.h
+++ OpenPanzerScout/Version.h
@@ -4,7 +4,7 @@
 #define VERSION_H
 
 
-#define FIRMWARE_VERSION        "0.91.10"      // Last update 7/1/2018
+#define FIRMWARE_VERSION        "0.91.11"      // Last update 4/30/2019
 
 // NOTE:
 // Versions past 0.91.xx are for Rev 11 and later boards only
